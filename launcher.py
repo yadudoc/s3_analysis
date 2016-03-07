99fcdb68c8d79802bb37587143f4c4eb784af63e
Yadu Nand Babuji <yadudoc1729@gmail.com>
2016-03-06 19:56:11 -0600

    Updates

--- launcher.py
+++ launcher.py
@@ -32,10 +32,10 @@ def start_instance(app, image_id, instance_type) :
                                                            count=1, 
                                                            type='one-time',
                                                            valid_until="{0}".format(deadline.isoformat()),
-                                                           #availability_zone_group=None, 
-                                                           #key_name="klab-mesos.us-east-1", 
-                                                           user_data=userdata, 
-                                                           instance_type=instance_type, 
+                                                           #availability_zone_group=None,
+                                                           #key_name="klab-mesos.us-east-1",
+                                                           user_data=userdata,
+                                                           instance_type=instance_type,
                                                            monitoring_enabled=False,
                                                            instance_profile_arn=role,
                                                            dry_run=DRY_RUN)
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         app.config.load_config("production.conf")
     except Exception as e:
         logging.error("Exception {0} in load_config".format(e))
-        exit(-1)    
+        exit(-1)
     cm.update_creds_from_metadata_server(app)
 
     instances = ["m4.large", "m4.xlarge", "m4.10xlarge", "c4.xlarge", "c4.8xlarge"]
@@ -61,3 +61,4 @@ if __name__ == "__main__":
             print app.config["ec2.conn"]
             status = start_instance(app, m["ami"], instance)
             print "{0} {1} {2}".format(m["region_code"], instance, status)
+            break;
--- perf_results/plotter.py
+++ perf_results/plotter.py
@@ -82,27 +82,93 @@ def plotter_latency (results, instype):
         colors[ridx],  label=LABEL)
         '''
         omp1           =  plt.plot(datapoints,
-                                    [a/b for a,b in zip(datapoints,data_avg)],
-                                    linestyle='solid',
-                                    marker=markers[ridx],
-                                    markersize=5.0,
-                                    color=colors[ridx],
-                                    label=LABEL)
+                                   #data_avg,
+                                   [8*(a/b) for a,b in zip(datapoints,data_min)],
+                                   linestyle='solid',
+                                   marker=markers[ridx],
+                                   markersize=5.0,
+                                   color=colors[ridx],
+                                   label=LABEL)
 
         plt.setp(omp1, alpha=0.6, antialiased=True, linewidth=3.0)
 
 
     #plt.yscale('log', basex=10)
     plt.xticks([x for x in datapoints], [x for x in datapoints])
-    plt.xscale('log', basex=2)
+    #plt.xscale('log', basex=2)
+    #plt.yscale('log', basex=2)
     plt.legend(loc=2, ncol=10,  borderaxespad=2)
     plt.ylabel("Time to completion in seconds")
-    plt.xlabel("Size of Transferred data (MB)")
+    plt.xlabel("Bandwidth in Mbps")
+    #plt.suptitle('afoooasdsad', fontsize=16)
+    plt.show()
+
+def plotter_region (results, region):
+
+    figure         = plt.figure()
+
+    datapoints     = [1, 4, 16, 64, 256, 1024]
+
+    colors  = ['g', 'r', 'b', 'k', 'c', 'm', 'y']
+    markers = ['^', 'x', 'o', 'h', '+']
+
+    for ridx, instype in enumerate(results[region]):
+ 
+        data_min = [0 for i in datapoints]
+        data_max = [0 for i in datapoints]
+        data_avg = [0 for i in datapoints]
+        data_std = [0 for i in datapoints]
+
+        for idx, size in enumerate(datapoints):
+            data_min[idx] = min(results[region][instype]["download"][size])
+            data_max[idx] = max(results[region][instype]["download"][size])
+            data_avg[idx] = numpy.mean(results[region][instype]["download"][size])
+            data_std[idx] = numpy.std(results[region][instype]["download"][size])
+
+        print region
+        print results[region][instype]["download"][1024]
+        print datapoints, len(datapoints)
+        print data_avg, len(data_avg)
+
+        LABEL          = "{0} {1} {2}".format(region, instype, "Download")
+        '''
+        omp1           =  plt.plot(datapoints,
+                                   [a/b for a,b in zip(datapoints,data_avg)],
+        colors[ridx],  label=LABEL)
+        '''
+        omp1           =  plt.plot(datapoints,
+                                   #data_avg,
+                                   [8*(a/b) for a,b in zip(datapoints,data_min)],
+                                   linestyle='solid',
+                                   marker=markers[ridx],
+                                   markersize=10.0,
+                                   markerfacecoloralt=colors[ridx+1],
+                                   color=colors[ridx],
+                                   label=LABEL)
+
+        plt.setp(omp1, alpha=0.6, antialiased=True, linewidth=5.0)
+
+
+    #plt.yscale('log', basex=10)
+    plt.xticks([x for x in datapoints], [x for x in datapoints])
+    plt.xscale('log', basex=4)
+    #plt.yscale('log', basex=2)
+    plt.legend(loc=2, ncol=12,  borderaxespad=2)
+    plt.tight_layout()
+    plt.ylabel("Bandwidth in Mbps")
+    plt.xlabel("Data Size in MB")
     #plt.suptitle('afoooasdsad', fontsize=16)
     plt.show()
 
 
 instance_types = ["m4.large", "m4.xlarge", "m4.10xlarge", "c4.xlarge", "c4.8xlarge"]
-plotter_latency(results, instance_types[2])
+#plotter_latency(results, instance_types[2])
+#plotter_latency(results, instance_types[4])
+plotter_region(results, "us-east-1b")
+plotter_region(results, "eu-central-1a")
+plotter_region(results, "us-west-2c")
+
+#plotter_region(results, "eu-central-1a")
+
 #plotter_tiling()
 exit()
